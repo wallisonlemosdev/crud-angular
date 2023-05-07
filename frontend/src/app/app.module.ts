7acb56b7b76bec4353db7f650deaeee57e478c8c
Wallison Lemos <w.alexandrelemos@gmail.com>
2023-05-07 18:51:57 -0300

    Componente atualizar produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -43,7 +44,8 @@ registerLocaleData(localePt);
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
